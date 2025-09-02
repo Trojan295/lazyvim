4cada0bf6358b249949232c8e4933af709f6db97
Damian Czaja <damian@cast.ai>
2025-09-02 18:29:59 +0200

    add debugging and testing

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -51,4 +51,55 @@ return {
       },
     },
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts_extend = { "ensure_installed_version", "ensure_installed" },
+    opts = {
+      ensure_installed_version = {
+        { "golangci-lint", "v1.64.8" },
+        { "golangci-lint-langserver", "v0.0.10" },
+      },
+    },
+    ---@param opts MasonSettings | {ensure_installed: string[], ensure_installed_version: string[][]}
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load this newly installed LSP server
+          require("lazy.core.handler.event").trigger({
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
+
+      mr.refresh(function()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+
+        -- for _, tool in ipairs(opts.ensure_installed_version) do
+        --   local p = mr.get_package(tool[0])
+        --   if not p:is_installed() then
+        --     p:install({
+        --       version = tool[1],
+        --     })
+        --   end
+        -- end
+      end)
+    end,
+  },
 }
